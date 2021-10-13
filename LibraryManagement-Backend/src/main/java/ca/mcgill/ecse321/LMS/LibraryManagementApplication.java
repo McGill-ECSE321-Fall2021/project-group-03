c11c0789fd9319fc0540ed3ad2f13fe9ddb9a7c3
Sam Vasserman <samvasserman@Sams-MacBook-Pro.local>
2021-10-13 15:40:49 -0400

    Initial commit of the backend applicationn

---
+++ LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/LMS/LibraryManagementApplication.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
+
+}
---
+++ LibraryManagement-Backend/src/test/java/ca/mcgill/ecse321/LMS/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
