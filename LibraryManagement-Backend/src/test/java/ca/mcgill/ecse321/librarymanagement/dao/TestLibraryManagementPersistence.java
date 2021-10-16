167320ef438a1fe103c45db5cc63b984e1a6210f
Sam Vasserman <samvasserman@Sams-MacBook-Pro.local>
2021-10-16 17:20:06 -0400

    saturday oct 16

--- LibraryManagement-Backend/src/test/java/ca/mcgill/ecse321/librarymanagement/dao/TestLibraryManagementPersistence.java
+++ LibraryManagement-Backend/src/test/java/ca/mcgill/ecse321/librarymanagement/dao/TestLibraryManagementPersistence.java
@@ -10,13 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import ca.mcgill.ecse321.LMS.LibraryManagementApplication;
 import ca.mcgill.ecse321.librarymanagement.model.Book;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = LibraryManagementApplication.class)
+@SpringBootTest
 public class TestLibraryManagementPersistence {
 
 	@Autowired
