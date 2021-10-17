e9d1260fbea46e25f58077a7d0589c97fe8c1d6a
Sam Vasserman <samvasserman@Sams-MacBook-Pro.local>
2021-10-17 17:23:03 -0400

    working on title persistence tests

--- LibraryManagement-Backend/src/test/java/ca/mcgill/ecse321/librarymanagement/dao/TestLibraryManagementPersistence.java
+++ LibraryManagement-Backend/src/test/java/ca/mcgill/ecse321/librarymanagement/dao/TestLibraryManagementPersistence.java
@@ -19,6 +19,8 @@ import ca.mcgill.ecse321.librarymanagement.LibraryManagementApplication;
 import ca.mcgill.ecse321.librarymanagement.model.Book;
 import ca.mcgill.ecse321.librarymanagement.model.Library;
 import ca.mcgill.ecse321.librarymanagement.model.LibrarySchedule;
+import ca.mcgill.ecse321.librarymanagement.model.Movie;
+import ca.mcgill.ecse321.librarymanagement.model.MusicAlbum;
 import ca.mcgill.ecse321.librarymanagement.model.Newspaper;
 import ca.mcgill.ecse321.librarymanagement.model.Room;
 import ca.mcgill.ecse321.librarymanagement.model.RoomSchedule;
@@ -135,43 +137,108 @@ public class TestLibraryManagementPersistence {
 	@Test
 	public void testPersistAndLoadNewspaper() {
 		
-				//Create all constructor fields
-				Date date = new Date(2001, 5, 1);
-				String image = "imageLink";
-				String newspaperName = "newspaperName";
-				String headline = "headline";
+		//Create all constructor fields
+		Date date = new Date(2001, 5, 1);
+		String image = "imageLink";
+		String newspaperName = "newspaperName";
+		String headline = "headline";
+		
+		//Create newspaper object with parameters. ^^
+		Newspaper newspaper = new Newspaper(date, image, newspaperName, headline);
 				
-				//Create newspaper object with parameters. ^^
-				Newspaper newspaper = new Newspaper(date, image, newspaperName, headline);
+		//Save the newspaper to the DB.
+		Newspaper savedNewspaper = newspaperRepository.save(newspaper);
+		int newspaperId = savedNewspaper.getTitleId();
+	
+		//Get rid of the book.
+		newspaper = null;
+		
+		//Use the CRUD method to query the book from the DB. 
+		newspaper = newspaperRepository.findNewspaperByTitleId(newspaperId);
 						
-				//Save the newspaper to the DB.
-				Newspaper savedNewspaper = newspaperRepository.save(newspaper);
-				int newspaperId = savedNewspaper.getTitleId();
-			
-				//Get rid of the book.
-				newspaper = null;
-				
-				//Use the CRUD method to query the book from the DB. 
-				newspaper = newspaperRepository.findNewspaperByTitleId(newspaperId);
-								
-				//Check that the object was properly queried from DB.
-				assertNotNull(newspaper);
-				
-				//Test that all the data was properly saved.
-				assertEquals(date, newspaper.getReleaseDate());
-				assertEquals(image, newspaper.getImage());
-				assertEquals(newspaperName, newspaper.getName());
-				assertEquals(headline, newspaper.getHeadline());
+		//Check that the object was properly queried from DB.
+		assertNotNull(newspaper);
+		
+		//Test that all the data was properly saved.
+		assertEquals(date, newspaper.getReleaseDate());
+		assertEquals(image, newspaper.getImage());
+		assertEquals(newspaperName, newspaper.getName());
+		assertEquals(headline, newspaper.getHeadline());
 		
 	}
 	
 	@Test
 	public void testPersistAndLoadMusicAlbum() {
 		
+		//Create all constructor fields
+		Date date = new Date(2001, 5, 1);
+		String image = "imageLink";
+		String musicAlbumName = "musicAlbumName";
+		String artist = "artist";
+		int duration = 60;
+		String genre = "genre";
+		
+		//Create album object with parameters. ^^
+		MusicAlbum album = new MusicAlbum(date, image, musicAlbumName, artist, duration, genre);
+		
+		//Save the album to the DB.
+		MusicAlbum savedAlbum = musicAlbumRepository.save(album);
+		int albumId = savedAlbum.getTitleId();
+			
+		//Get rid of the album.
+		album = null;
+		
+		//Use the CRUD method to query the album from the DB. 
+		album = musicAlbumRepository.findMusicAlbumByTitleId(albumId);
+						
+		//Check that the object was properly queried from DB.
+		assertNotNull(albumId);
+		
+		//Test that all the data was properly saved.
+		assertEquals(date, album.getReleaseDate());
+		assertEquals(image, album.getImage());
+		assertEquals(musicAlbumName, album.getName());
+		assertEquals(artist, album.getArtist());
+		assertEquals(duration, album.getDuration());
+		assertEquals(genre, album.getGenre());
 	}
 	
 	@Test
 	public void testPersistAndLoadMovie() {
+		
+		//Create all constructor fields
+		Date date = new Date(2001, 5, 1);
+		String image = "imageLink";
+		String movieName = "movieName";
+		String director = "director";
+		int duration = 60;
+		String synopsis = "synopsis";
+		String genre = "genre";
+		
+		//Create movie object with parameters. ^^
+		Movie movie = new Movie(date, image, movieName, director, genre, synopsis, duration);
+				
+		//Save the movie to the DB.
+		Movie savedMovie = movieRepository.save(movie);
+		int movieId = savedMovie.getTitleId();
+	
+		//Get rid of the movie.
+		movie = null;
+		
+		//Use the CRUD method to query the book from the DB. 
+		movie = movieRepository.findMovieByTitleId(movieId);
+						
+		//Check that the object was properly queried from DB.
+		assertNotNull(movie);
+		
+		//Test that all the data was properly saved.
+		assertEquals(date, movie.getReleaseDate());
+		assertEquals(image, movie.getImage());
+		assertEquals(movieName, movie.getName());
+		assertEquals(director, movie.getDirector());
+		assertEquals(duration, movie.getDuration());
+		assertEquals(genre, movie.getGenre());
+		assertEquals(synopsis, movie.getSynopsis());
 		
 	}
 	
