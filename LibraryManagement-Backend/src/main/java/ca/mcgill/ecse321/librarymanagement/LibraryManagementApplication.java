901a63664fc253b7b620f6a092f9dca8cb49eec1
avi <abraham.somech@mail.mcgill.ca>
2021-10-18 16:50:15 -0400

    trying to fix the build

--- LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/LibraryManagementApplication.java
+++ LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/LibraryManagementApplication.java
@@ -1,14 +1,17 @@
 package ca.mcgill.ecse321.librarymanagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @SpringBootApplication
 public class LibraryManagementApplication {
-
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApplication.class, args);
 	}
