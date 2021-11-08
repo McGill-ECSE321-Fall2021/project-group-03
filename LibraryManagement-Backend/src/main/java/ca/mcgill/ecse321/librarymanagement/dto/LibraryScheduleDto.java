d93f4b49378603850ac5bf1f6800355278141c78
Adam Geenen <adamgeenen@gmail.com>
2021-11-08 11:49:19 -0500

    removed unnecessary imports

--- LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/dto/LibraryScheduleDto.java
+++ LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/dto/LibraryScheduleDto.java
@@ -2,7 +2,6 @@ package ca.mcgill.ecse321.librarymanagement.dto;
 
 import java.util.List;
 
-import ca.mcgill.ecse321.librarymanagement.model.Room;
 import ca.mcgill.ecse321.librarymanagement.model.TimeSlot;
 
 public class LibraryScheduleDto {
