98a5fead9ad67ddfaf7130483b0bc38139afb616
avi-somech <abraham.somech@mail.mcgill.ca>
2021-11-28 22:05:27 -0500

    fixed room reservations with Liam

--- LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/model/Library.java
+++ LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/model/Library.java
@@ -1,6 +1,8 @@
 package ca.mcgill.ecse321.librarymanagement.model;
 /*PLEASE DO NOT EDIT THIS CODE*/
 
+import java.sql.Date;
+import java.sql.Time;
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
 import java.util.ArrayList;
 import java.util.Collections;
