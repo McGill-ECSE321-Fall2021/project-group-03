0b56b8a3eff5e5ebbdc46cb201c0c354530db561
Sam Vasserman <samvasserman@Sams-MacBook-Pro-2.local>
2021-11-30 12:37:00 -0500

    more

--- LibraryManagement-Frontend/src/views/home.js
+++ LibraryManagement-Frontend/src/views/home.js
@@ -11,12 +11,17 @@ var AXIOS = axios.create({
   headers: { 'Access-Control-Allow-Origin': frontendUrl }
 })
 
-
-
 export default {
   name: "Home",
   components: {
     MenuBar,
+  },
+
+  data(){
+      return {
+          librarySchedule: [],
+          response: []
+      }
   },
   mounted: function loadLibrarySchedule(){
         const url = "/libraryTimeslots/get"
@@ -24,37 +29,18 @@ export default {
         // create array of the next 7 days
         var today = new Date()
         today.setDate(today.getDate()+1)
-        var thisWeek = [
+        var thisWeek = []
         
         for (let i = 0 ; i < 7 ; i++){
             var nextDay = new Date()
             nextDay.setDate(today.getDate()+i)
             const day = (nextDay.getDate() < 10) ? "0" + nextDay.getDate() : nextDay.getDate()
             thisWeek.push(nextDay.getFullYear()+'-'+(nextDay.getMonth()+2)+'-'+day)
-        }       
-        AXIOS.get(url)
-        .then(response => {
+        }
+
+        AXIOS.get(url).then(response => {
             // JSON responses are automatically parsed.
-            let r = response.data
-            (r).forEach(timeslot => {
-
-                // parse date
-                const date = timeslot.date
-                const year = "20" + date.split("-")[0].substring(2,4)
-                const month = date.split("-")[1]
-                const day = date.split("-")[2]
-                const timeslotDate = year+'-'+month+'-'+day
-
-                // parse times
-                const startTime = timeslot.startTime
-                const endTime = timeslot.endTime
-
-                console.log(startTime)
-                console.log(endTime)
-
-
-
-            });
+            this.librarySchedule = response.data
         })
         .catch(e => {
             this.errorTitle = e
