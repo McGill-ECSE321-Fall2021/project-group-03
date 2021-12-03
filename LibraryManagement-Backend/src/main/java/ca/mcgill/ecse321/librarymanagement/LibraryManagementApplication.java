1de6860d20601ff6121805f7e3cb3f0b971ce372
Sam Vasserman <samvasserman@Sams-MacBook-Pro-2.local>
2021-12-03 14:07:46 -0500

    Update LibraryManagementApplication.java

--- LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/LibraryManagementApplication.java
+++ LibraryManagement-Backend/src/main/java/ca/mcgill/ecse321/librarymanagement/LibraryManagementApplication.java
@@ -1,14 +1,25 @@
 package ca.mcgill.ecse321.librarymanagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.librarymanagement.controller.LibraryManagementRestController;
+import ca.mcgill.ecse321.librarymanagement.dto.TitleDto;
+import ca.mcgill.ecse321.librarymanagement.model.Library;
+import ca.mcgill.ecse321.librarymanagement.model.Title;
 
 @RestController
 @SpringBootApplication
 public class LibraryManagementApplication {
 	
+	@Autowired
+	private static LibraryManagementRestController controller;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApplication.class, args);
@@ -19,6 +30,14 @@ public class LibraryManagementApplication {
 	@RequestMapping("/")
 	public String greeting(){		
 		return "Hello world this is a test!";
+	}
+	
+	@RequestMapping(value = { "/titles/create/{name}", "/titles/create/{name}/" })
+	public TitleDto createTitle(@PathVariable("name") String name, @RequestParam String description,
+			@RequestParam String genre, @RequestParam String isAvailable, @RequestParam String titleType)
+			throws IllegalArgumentException {
+
+		return controller.createTitle(name, description, genre, isAvailable, titleType);
 	}
 
 }
